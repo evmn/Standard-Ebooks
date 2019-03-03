f1d47ec3aee0423c617e24d9cfb5481c036cefad
evmn <afls@yandex.com>
2019-03-03 16:12:50 +0800

    get book meta from standardebooks.org

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Standard Ebooks
+
+
+Save book info to local sqlite3 database.
+
+|ID|Title|Author|Words|Ease|Description|
+|:-|:-|:-|:-|:-|:-|
+1|The Time Machine|H. G. Wells|32575|71.04
+2|The Lady of the Barge|W. W. Jacobs|47269|82.34
+3|Candide|Voltaire|33776|67.89
+4|Heart of Darkness|Joseph Conrad|38791|71.44
+5|The Book of Wonder|Lord Dunsany|23006|66.3
+6|Beyond Good and Evil|Friedrich Nietzsche|63621|34.83
+7|The Call of the Wild|Jack London|32031|77.37
+8|The Mysterious Affair at Styles|Agatha Christie|57825|77.03
+9|White Fang|Jack London|72797|81.22
+10|The Narrative of Arthur Gordon Pym of Nantucket|Edgar Allan Poe|70791|51.21
+...|...|...|...|...|...
+...|...|...|...|...|...
+242|Greenmantle|John Buchan|100167|81.53
+
+
+```sh
+python3 meta.py
+```
---
+++ meta.py
@@ -0,0 +1,67 @@
+from urllib.request import urlopen
+import re, time, os, sys
+from bs4 import BeautifulSoup
+import sqlite3
+
+# Returns a list of URLs to books listed on the standardebooks.org website.
+def getBookPages():
+	ebook_urls = []
+	base_url = "https://standardebooks.org/ebooks/?page="
+	root_url = "https://standardebooks.org"
+	for i in range(1, 22):
+		url = base_url + str(i)
+		html = urlopen(url)
+		bsObj = BeautifulSoup(html, "lxml")
+		titleList = bsObj.find('ol').findAll('li')
+		for link in titleList:
+			ebook_urls.extend([root_url + link.find('a').get('href')])
+	return ebook_urls
+
+def saveMetainfo(links):
+	db.execute("select * from books")
+	localBooks = len(db.fetchall())
+	serverBooks = len(links)
+	print('\n\nThere are', serverBooks, 'books in StandardEbooks.org')
+	print('there are', localBooks, 'books in local database\n')
+	start = serverBooks - localBooks
+	if start <= 0:
+		print('no need to download')
+		return
+	print('We will download', start, 'books this time')
+	#Traverse the list in reverse order
+	#listObj[start:stop:step]
+	for url in links[start-1::-1]:
+		html = urlopen(url)
+		bsObj = BeautifulSoup(html, "lxml")
+		title = bsObj.find('article').find('h1').get_text()
+		author = bsObj.find('article').find('p').get_text()
+		#image = bsObj.find('article').find('img')
+		reading_ease = bsObj.find('aside', attrs={"id":"reading-ease"}).find('p').get_text()
+		words = getWords(reading_ease)
+		readingeasy = getReadingEasy(reading_ease)
+		description = bsObj.find('section', attrs={"id":"description"}).get_text().replace("Description\n","")
+		db.execute('''insert into books(Title, Author, Words, Ease, Description)  values(?,?,?,?,?)''', (title, author, words, readingeasy, description))
+		print(title, "-", author, "\t", words, "\t", readingeasy)
+		#print(description)
+
+def getWords(readingeasy):
+	tmpList = re.findall(r'[\d,\.]+', readingeasy)
+	words = int(tmpList[0].replace(",", ""))
+	return words
+
+def getReadingEasy(readingeasy):
+	tmpList = re.findall(r'[\d,\.]+', readingeasy)
+	easy = float(tmpList[len(tmpList)-1])
+	return easy
+
+conn = sqlite3.connect('StandardEbooks.db')
+db = conn.cursor()
+db.execute('''create table if not exists books 
+	(ID integer primary key autoincrement,
+	Title text, 
+	Author text,
+	Words int,
+	Ease float,
+	Description text)''')
+saveMetainfo(getBookPages())
+conn.commit()
