55c9dcc1a28ae623a78de0f7e34f563ef68436f6
evmn <afls@yandex.com>
2019-03-04 09:27:11 +0800

    query database and output to markdown format

---
+++ db2md.py
@@ -0,0 +1,18 @@
+import sqlite3
+def db2md():
+	conn = sqlite3.connect('StandardEbooks.db')
+	db = conn.cursor()
+	for books in db.execute('select * from books order by ease asc').fetchall():
+	#for books in db.execute('select * from books order by ease desc').fetchall():
+	#for books in db.execute('select * from books order by author').fetchall():
+		id = books[0]
+		title = books[1]
+		author = books[2]
+		words = books[3]
+		ease = books[4]
+		description = books[5]
+		print('##', author, '-', title)
+		print('\n', 'There are', words, 'words in this book.\n')
+		print('### Description\n', description)
+
+db2md()
